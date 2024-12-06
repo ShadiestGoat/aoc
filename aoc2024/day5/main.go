056f193f935070394bba85bb0bc140a0f28907fb
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2024-12-06 17:10:05 +0000

    2024 Day 5

---
+++ aoc2024/day5/main.go
@@ -0,0 +1,138 @@
+package day5
+
+import (
+	"strings"
+
+	"github.com/shadiestgoat/aoc/utils"
+)
+
+type Config struct {
+	// num -> it's deps (ie. stuff that has to be before it)
+	NumDeps map[int][]int
+	Lists [][]int
+}
+
+func (c *Config) meetsDeps(n int, allVals map[int]bool, used map[int]bool) bool {
+	for _, d := range c.NumDeps[n] {
+		if !allVals[d] {
+			continue
+		}
+
+		if !used[d] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (c *Config) isListRight(l []int) bool {
+	used := map[int]bool{}
+	allVals := utils.MapListKeys(l)
+
+	for _, n := range l {
+		if !c.meetsDeps(n, allVals, used) {
+			return false
+		}
+
+		used[n] = true
+	}
+
+	return true
+}
+
+func (c *Config) makeListRight(l []int) []int {
+	allVals := utils.MapListKeys(l)
+	used := map[int]bool{}
+	receptacle := []int{}
+	newL := []int{}
+
+	doReceptacle := func () {
+		for {
+			newRec := []int{}
+
+			for _, r := range receptacle {
+				if c.meetsDeps(r, allVals, used) {
+					used[r] = true
+					newL = append(newL, r)
+					continue
+				}
+
+				newRec = append(newRec, r)
+			}
+
+			if len(newRec) == len(receptacle) {
+				return
+			}
+
+			receptacle = newRec
+		}
+	}
+
+	for _, n := range l {
+		doReceptacle()
+
+		if !c.meetsDeps(n, allVals, used) {
+			receptacle = append(receptacle, n)
+			continue
+		}
+
+		used[n] = true
+		newL = append(newL, n)
+	}
+
+	doReceptacle()
+
+	if len(receptacle) != 0 {
+		panic(":/")
+	}
+
+	return newL
+}
+
+func parseInput(inp string) *Config {
+	spl := strings.Split(inp, "\n\n")
+	cfg := &Config{
+		NumDeps: map[int][]int{},
+		Lists:   utils.SplitAndParseInt2(spl[1], "\n", ","),
+	}
+
+	for _, l := range strings.Split(spl[0], "\n") {
+		p := utils.SplitAndParseInt(l, "|")
+		cfg.NumDeps[p[1]] = append(cfg.NumDeps[p[1]], p[0])
+	}
+
+	return cfg
+}
+
+func Solve1(inp string) any {
+	cfg := parseInput(inp)
+	sum := 0
+
+	for _, l := range cfg.Lists {
+		if !cfg.isListRight(l) {
+			continue
+		}
+
+		sum += l[len(l)/2]
+	}
+
+	return sum
+}
+
+func Solve2(inp string) any {
+	cfg := parseInput(inp)
+	sum := 0
+
+	for _, l := range cfg.Lists {
+		if cfg.isListRight(l) {
+			continue
+		}
+
+		nl := cfg.makeListRight(l)
+
+		sum += nl[len(nl)/2]
+	}
+
+	return sum
+}
---
+++ aoc2024/day5/main_test.go
@@ -0,0 +1,47 @@
+package day5_test
+
+import (
+	"testing"
+
+	"github.com/shadiestgoat/aoc/aoc2024/day5"
+	"github.com/shadiestgoat/aoc/utils/tutils"
+)
+
+const INPUT = `
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+`
+
+func TestSolve1(t *testing.T) {
+	tutils.AssertFunc(t, INPUT, day5.Solve1, 143)
+}
+
+func TestSolve2(t *testing.T) {
+	tutils.AssertFunc(t, INPUT, day5.Solve2, 123)
+}
