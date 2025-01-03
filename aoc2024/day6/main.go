d91e85f51288cf7b8e844beb7d9fe677e386a293
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2025-01-03 13:47:55 +0000

    2024: Optimize day 6

--- aoc2024/day6/main.go
+++ aoc2024/day6/main.go
@@ -1,66 +1,20 @@
 package day6
 
 import (
-	"slices"
 	"strings"
 
+	"github.com/shadiestgoat/aoc/utils/sillyset/set3"
 	"github.com/shadiestgoat/aoc/utils/xy"
 )
 
 type State struct {
-	// Coord -> Directions
-	DirHist map[xy.XY][]xy.XY
-	CurPos  xy.XY
-	Dir     xy.XY
-	Board   []string
-}
+	CurPos xy.XY
+	OgPos  xy.XY
+	Dir    xy.XY
 
-func (s State) outOfBounds(c xy.XY) bool {
-	return c.OutOfBounds(xy.XY{len(s.Board[0]), len(s.Board)})
-}
-
-// Does 1 action. Either moves or rotates
-// Returns true if goes of the map
-// Doesn't record history
-func (s *State) GoOnce() bool {
-	nextV := s.CurPos.Add(s.Dir)
-
-	if s.outOfBounds(nextV) {
-		return true
-	}
-
-	if s.Board[nextV[1]][nextV[0]] == '#' {
-		s.Dir = s.Dir.RotateUnitVector(2)
-
-		return false
-	}
-
-	s.CurPos = nextV
-
-	return false
-}
-
-func (s *State) GoUntilExit() {
-	for !s.GoOnce() {
-		s.DirHist[s.CurPos] = append(s.DirHist[s.CurPos], s.Dir)
-	}
-}
-
-func (s *State) findObstacleInDirection(og xy.XY, dir xy.XY) bool {
-	i := 1
-	for {
-		pos := og.Add(dir.Mul(i))
-
-		if s.outOfBounds(pos) {
-			return false
-		}
-
-		if s.Board[pos[1]][pos[0]] == '#' {
-			return true
-		}
-
-		i++
-	}
+	size       xy.XY
+	WallsByCol []set3.Set
+	WallsByRow []set3.Set
 }
 
 func parseInput(inp string) *State {
@@ -68,82 +22,115 @@ func parseInput(inp string) *State {
 	// Its been a tiring week... I'm not gonna bother with a fully string board like aoc2020/day11
 	lines := strings.Split(strings.Replace(inp, "^", ".", 1), "\n")
 	perLineChars := len(lines[0]) + 1
-
 	pos := xy.XY{rawPos % perLineChars, rawPos / perLineChars}
 
+	obsByCol := make([]set3.Set, len(lines[0]))
+	obsByRow := make([]set3.Set, len(lines))
+
+	for y, l := range lines {
+		for x, v := range l {
+			if v == '#' {
+				obsByCol[x] = obsByCol[x].UpdateAt(uint64(y), 1)
+				obsByRow[y] = obsByRow[y].UpdateAt(uint64(x), 1)
+			}
+		}
+	}
+
 	return &State{
-		DirHist: map[xy.XY][]xy.XY{
-			pos: {
-				{0, -1},
-			},
-		},
-		CurPos: pos,
-		Dir:    xy.XY{0, -1},
-		Board:  lines,
+		CurPos:     pos,
+		OgPos:      pos,
+		Dir:        xy.DIR_UP,
+		WallsByCol: obsByCol,
+		WallsByRow: obsByRow,
+		size:       xy.GetSizeString(lines),
+	}
+}
+
+func (s *State) GoUntilExit() map[xy.XY]bool {
+	pos := map[xy.XY]bool{}
+
+	for {
+		nextV := s.CurPos.Add(s.Dir)
+		if nextV.OutOfBounds(s.size) {
+			return pos
+		}
+
+		if s.WallsByCol[nextV[0]].At(uint64(nextV[1])) {
+			s.Dir = s.Dir.RotateVector(2)
+
+			continue
+		}
+
+		s.CurPos = nextV
+		pos[s.CurPos] = true
 	}
 }
 
 func Solve1(inp string) any {
-	s := parseInput(inp)
-	s.GoUntilExit()
+	return len(parseInput(inp).GoUntilExit())
+}
 
-	return len(s.DirHist)
+func (s *State) AddWall(c xy.XY, v uint64) {
+	s.WallsByCol[c[0]] = s.WallsByCol[c[0]].UpdateAt(uint64(c[1]), v)
+	s.WallsByRow[c[1]] = s.WallsByRow[c[1]].UpdateAt(uint64(c[0]), v)
+}
+
+func (s State) DoesLoop() bool {
+	curPos := s.OgPos
+	curDir := xy.DIR_UP
+
+	hist := map[[2]xy.XY]bool{
+		{curPos, curDir}: true,
+	}
+
+	for {
+		var walls set3.Set
+		var i int
+
+		if curDir[0] == 0 {
+			// Going vertically
+			walls = s.WallsByCol[curPos[0]]
+			i = 1
+		} else {
+			// Going horizontally
+			walls = s.WallsByRow[curPos[1]]
+			i = 0
+		}
+
+		var f int
+		if curDir[i] < 0 {
+			f = walls.ClosestSmall(curPos[i])
+		} else {
+			f = walls.ClosestBig(curPos[i])
+		}
+		if f == -1 {
+			return false
+		}
+
+		curPos[i] = f - curDir[i]
+		curDir = curDir.RotateVector(2)
+		k := [2]xy.XY{curPos, curDir}
+
+		if hist[k] {
+			return true
+		}
+		hist[k] = true
+	}
 }
 
 func Solve2(inp string) any {
 	s := parseInput(inp)
-
-	ogPos := s.CurPos
-	ogBoard := make([]string, len(s.Board))
-	copy(ogBoard, s.Board)
-
-	s.GoUntilExit()
-
-	oldHist := s.DirHist
-	delete(oldHist, ogPos)
-	testHist := map[xy.XY]bool{}
-	dirs := []xy.XY{
-		{0, -1},
-		{0, 1},
-		{-1, 0},
-		{1, 0},
-	}
-
-	for c := range oldHist {
-		for _, d := range dirs {
-			if s.findObstacleInDirection(c, d) {
-				testHist[c] = true
-				break
-			}
-		}
-	}
+	hist := s.GoUntilExit()
 
 	amt := 0
+	for c := range hist {
+		s.AddWall(c, 1)
 
-	for c := range testHist {
-		// Reset to og
-		s.DirHist = map[xy.XY][]xy.XY{
-			ogPos: {
-				{0, -1},
-			},
+		if s.DoesLoop() {
+			amt++
 		}
 
-		s.Dir = xy.XY{0, -1}
-		s.CurPos = ogPos
-		s.Board = make([]string, len(s.Board))
-		copy(s.Board, ogBoard)
-
-		row := s.Board[c[1]]
-		s.Board[c[1]] = row[:c[0]] + "#" + row[c[0]+1:]
-
-		for !s.GoOnce() {
-			if slices.Contains(s.DirHist[s.CurPos], s.Dir) {
-				amt++
-				break
-			}
-
-			s.DirHist[s.CurPos] = append(s.DirHist[s.CurPos], s.Dir)
-		}
+		s.AddWall(c, 0)
 	}
 
 	return amt
